09e8465f2a7c067f94a61a63b729c7dd38a7ccc8
Татьяна <robsten9086@mail.ru>
2023-07-09 17:53:02 +0300

    задача 13

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+
+        if (input.Length >= 3)
+        {
+            int number = int.Parse(input);
+            int thirdDigit = (number / 100) % 10;
+            Console.WriteLine("Третья цифра числа: " + thirdDigit);
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет.");
+        }
+    }
+}
